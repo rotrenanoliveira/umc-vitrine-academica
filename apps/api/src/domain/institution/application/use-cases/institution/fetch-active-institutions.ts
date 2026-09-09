78ccdf0931936905a06dd3cdf9df934f2e2aa69e
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-09-09 15:37:29 -0300

    fix: return `unkown` at use cases

--- apps/api/src/domain/institution/application/use-cases/institution/fetch-active-institutions.ts
+++ apps/api/src/domain/institution/application/use-cases/institution/fetch-active-institutions.ts
@@ -2,13 +2,16 @@ import { type Either, right } from '@/core/either'
 import type { Institution } from '../../../enterprise/entities/institution'
 import type { InstitutionsRepository } from '../../repositories/institutions-repository'
 
-type FetchActiveInstitutionsResponse = Either<never, { institutions: Institution[] }>
+type FetchActiveInstitutionsResponse = Either<unknown, { institutions: Institution[] }>
 
 export class FetchActiveInstitutionsUseCase {
   constructor(private readonly institutionsRepository: InstitutionsRepository) {}
 
   async execute(): Promise<FetchActiveInstitutionsResponse> {
     const institutions = await this.institutionsRepository.findManyActive()
-    return right({ institutions })
+
+    return right({
+      institutions,
+    })
   }
 }
--- apps/api/src/domain/institution/application/use-cases/institution/fetch-institutions.ts
+++ apps/api/src/domain/institution/application/use-cases/institution/fetch-institutions.ts
@@ -2,13 +2,16 @@ import { type Either, right } from '@/core/either'
 import type { Institution } from '../../../enterprise/entities/institution'
 import type { InstitutionsRepository } from '../../repositories/institutions-repository'
 
-type FetchInstitutionsResponse = Either<never, { institutions: Institution[] }>
+type FetchInstitutionsResponse = Either<unknown, { institutions: Institution[] }>
 
 export class FetchInstitutionsUseCase {
   constructor(private readonly institutionsRepository: InstitutionsRepository) {}
 
   async execute(): Promise<FetchInstitutionsResponse> {
     const institutions = await this.institutionsRepository.findAll()
-    return right({ institutions })
+
+    return right({
+      institutions,
+    })
   }
 }
