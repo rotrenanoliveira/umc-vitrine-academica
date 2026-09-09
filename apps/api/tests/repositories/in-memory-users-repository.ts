6576f2b0c0b4a80ce937c968fe2a06a58aa2d8ab
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-09-09 19:48:55 -0300

    feat: define new method at users repository interface

--- apps/api/tests/repositories/in-memory-users-repository.ts
+++ apps/api/tests/repositories/in-memory-users-repository.ts
@@ -8,6 +8,10 @@ export class InMemoryUsersRepository implements UsersRepository {
     return this.items.find((user) => user.email === email) || null
   }
 
+  async findById(id: string): Promise<User | null> {
+    return this.items.find((user) => user.id.toString() === id) || null
+  }
+
   async create(user: User): Promise<void> {
     this.items.push(user)
   }
